dc544fc483bb2f32658c5ce0f362039eb016c635
ogapy <kyon6690@gmail.com>
2024-12-26 01:40:02 +0900

    format terraform

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name = "${var.project}-terraform-state-lock"
-  hash_key = "LockID"
+  name         = "${var.project}-terraform-state-lock"
+  hash_key     = "LockID"
   billing_mode = "PAY_PER_REQUEST" # オンデマンド
 
   attribute {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,28 +2,28 @@ terraform {
   required_version = "~> 1.4.6"
 
   backend "s3" {
-    bucket = "ogapy-snowflake-terraform"
-    region = "ap-northeast-1"
-    key = "common-snowflake.tfstate"
+    bucket         = "ogapy-snowflake-terraform"
+    region         = "ap-northeast-1"
+    key            = "common-snowflake.tfstate"
     dynamodb_table = "ogawa-terraform-state-lock"
-  } 
+  }
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.47.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   profile = "private"
   default_tags {
     tags = {
-      env = var.environment
+      env     = var.environment
       project = var.project
-      owner = "terraform"
+      owner   = "terraform"
       tfstate = "common-github"
     }
   }
